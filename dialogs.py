648c10a15087e3e22a22252fc63b5e82847db965
kiranwoth <KiranWotherspoon@outlook.com>
2023-06-20 19:55:30 -0400

    added dialog practice

---
+++ dialogs.py
@@ -0,0 +1,27 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QDialog
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        button = QPushButton("Press me for a dialog!")
+        button.clicked.connect(self.button_clicked)
+        self.setCentralWidget(button)
+
+    def button_clicked(self, s):
+        print("click", s)
+
+        dlg = QDialog(self)
+        dlg.setWindowTitle("HELLO!")
+        dlg.exec()
+
+
+app = QApplication([])
+
+window = MainWindow()
+window.show()
+
+app.exec()
